118b9c67bb660bb7495da81fc0d69fb7aaeab104
何喜 <877073978@qq.com>
2021-01-07 16:46:41 +0800

    获取用户信息,保存token功能实现

--- admin/js/login.js
+++ admin/js/login.js
@@ -12,7 +12,7 @@ $(function () {
             $('#myModal').modal()
             $('.modal-body>p').text('帐号或密码不能为空')
         } else {
-            // 发送ajax请求
+            // 5.发送ajax请求
             $.ajax({
                 url: 'http://localhost:8080/api/v1/admin/user/login',
                 type: 'post',
@@ -24,6 +24,9 @@ $(function () {
                     $('#myModal').modal()
                     $('.modal-body>p').text(backData.msg)
                     if (backData.code == 200) {
+                        //将服务器返回的token保存到localStorage
+                        localStorage.setItem('token', backData.token)
+                        //此事件在模态框被隐藏（并且同时在 CSS 过渡效果完成）之后被触发
                         $('#myModal').on('hidden.bs.modal', function (e) {
                             location.href = './index.html'
                         })
